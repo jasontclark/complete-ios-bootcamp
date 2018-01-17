6a599e5ad7e091b32bc783fb795a089f7d22e193
How, Swift? <github@howswift.com>
2018-01-17 05:35:29 -0500

    Created the updateDiceImages() func to randomly update the dice faces.

--- xcode-projects/xcode-dicee-app/Dicee/ViewController.swift
+++ xcode-projects/xcode-dicee-app/Dicee/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2 : Int = 0
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
@@ -19,6 +21,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        updateDiceImages()
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,12 +31,18 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
         
+        // Generate a random array index number.
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
         
-        print(randomDiceIndex1)
-        
+        // Use the random array index to generate a new dice face.
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
     
 }
