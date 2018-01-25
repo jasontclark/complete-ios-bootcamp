653920cc5ec3591a4a30eb915358de5ece71b28a
How, Swift? <github@howswift.com>
2018-01-25 05:31:16 -0500

    Section 8, Lecture 76 - Adding the AVAudioPlayer code to the xylophone app.

--- xcode-projects/xcode-xylophone-app/Xylophone/ VIewController.swift
+++ xcode-projects/xcode-xylophone-app/Xylophone/ VIewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate {
     
-
+    var audioPlayer : AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -18,12 +20,15 @@ class ViewController: UIViewController{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
+        let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
         
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        } catch {
+            print(error)
+        }
         
-        
+        audioPlayer.play()
     }
-    
-  
-
 }
 
