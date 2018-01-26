916fd24a872efc0e56e16fd2c496514ee0849860
How, Swift? <github@howswift.com>
2018-01-26 06:28:45 -0500

    Wrapping up Section 8 with the completed Xylophone app.

--- xcode-projects/xcode-xylophone-app/Xylophone/ VIewController.swift
+++ xcode-projects/xcode-xylophone-app/Xylophone/ VIewController.swift
@@ -11,23 +11,47 @@ import AVFoundation
 
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     
+    // AVFoundation AVAudioPlayer for playing sound
     var audioPlayer : AVAudioPlayer!
+    
+    // Array that holds the filenames for each note
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    
+    // When the button is pressed, determines which file to play
+    var selectedSoundFileName : String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
-
+    // Action that is triggered each time a button is pressed
     @IBAction func notePressed(_ sender: UIButton) {
-        let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
         
+        // Based on the tag attribute for each button, select an filename
+        // from the array. The -1 is so that we don't end up with an
+        // IndexOutOfBounds error
+        selectedSoundFileName = soundArray[sender.tag - 1]
+        
+        // Call the playSound() function
+        playSound()
+    }
+    
+    // Plays the wave file selected
+    func playSound() {
+        
+        // Creates the URL path to the sound
+        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+        
+        
+        // Exception/Error Handling do-try-catch block
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
         } catch {
             print(error)
         }
         
+        // Plays the file
         audioPlayer.play()
     }
 }
