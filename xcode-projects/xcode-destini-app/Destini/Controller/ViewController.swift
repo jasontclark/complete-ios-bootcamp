cecef44c46334924a581ad920bd5bf13188e40bd
How, Swift? <github@howswift.com>
2018-02-07 05:11:20 -0500

    Completed the destini-app challenge. Solved it using MVC.

--- xcode-projects/xcode-destini-app/Destini/Controller/ViewController.swift
+++ xcode-projects/xcode-destini-app/Destini/Controller/ViewController.swift
@@ -10,25 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // Our strings
-//    let story1 = "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\"."
-//    let answer1a = "I\'ll hop in. Thanks for the help!"
-//    let answer1b = "Better ask him if he\'s a murderer first."
-//
-//
-//    let story2 = "He nods slowly, unphased by the question."
-//    let answer2a = "At least he\'s honest. I\'ll climb in."
-//    let answer2b = "Wait, I know how to change a tire."
-//
-//    let story3 = "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box."
-//    let answer3a = "I love Elton John! Hand him the cassette tape."
-//    let answer3b = "It\'s him or me! You take the knife and stab him."
-//
-//    let story4 = "What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?"
-//    let story5 = "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in."
-//    let story6 = "You bond with the murderer while crooning verses of \"Can you feel the love tonight\". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: \"Try the pier.\""
-    
-    
     // UI Elements linked to the storyboard
     @IBOutlet weak var topButton: UIButton!         // Has TAG = 1
     @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
@@ -36,34 +17,63 @@ class ViewController: UIViewController {
     
     // TODO Step 5: Initialise instance variables here
     let allStories           = StoryBank()
-    let currentStoryNum: Int = 0
+    var currentStoryNum: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
-       nextStory()
-        
+        // Load the first story
+        nextStory()
     }
 
     
     // User presses one of the buttons
     @IBAction func buttonPressed(_ sender: UIButton) {
-    
-        // TODO Step 4: Write an IF-Statement to update the views
-                
-        // TODO Step 6: Modify the IF-Statement to complete the story
         
-    
+        // Load the current story
+        let currentStory = allStories.list[currentStoryNum]
+        
+        // Placeholder variable for the next story
+        var nextStoryNum = 0
+        
+        // TODO Step 4: Write an IF-Statement to update the views
+        if sender.tag == 1 {
+            // Use the getNextStory() function in the Story object
+            // when the answer option is A.
+            nextStoryNum = currentStory.getNextStory(answerChosen: "A")
+        } else {
+            // Use the getNextStory() function in the Story object
+            // when the answer option is B.
+            nextStoryNum = currentStory.getNextStory(answerChosen: "B")
+        }
+                
+        // Update the currentStoryNum with the nextStoryNum
+        currentStoryNum = nextStoryNum
+        
+        // Load the next story
+        nextStory()
     }
     
+    // nextStory() takes care of loading the next story on the view.
     func nextStory() {
-        
+        // Loads the next story from the StoryBank()
         let currentStory = allStories.list[currentStoryNum]
+        
+        // Updates the story text on the view
         storyTextView.text = currentStory.storyText
-        topButton.setTitle(currentStory.aAnswer, for: UIControlState.normal)
-        bottomButton.setTitle(currentStory.bAnswer, for: UIControlState.normal)
+        
+        // Checks to see if the buttons need to be loaded
+        // If so, the text is updated on them.
+        if currentStoryNum <= 2 {
+            // Only the first 3 stories have answer choices.
+            topButton.setTitle(currentStory.aAnswer, for: UIControlState.normal)
+            bottomButton.setTitle(currentStory.bAnswer, for: UIControlState.normal)
+        } else {
+            // The 4th, 5th, and 6th stories do not.
+            topButton.isHidden    = true
+            bottomButton.isHidden = true
+        }
+        
     }
 
 }
