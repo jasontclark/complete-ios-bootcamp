84592c74051301c91ee948c468380684589dd429
How, Swift? <github@howswift.com>
2018-02-23 07:31:04 -0500

    Adding the segues project for learning how to navigate from one ViewController to another, along with passing information from one ViewController to Another.

---
+++ xcode-projects/xcode-segues/Segues/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Segues
+//
+//  Created by Jay Clark on 2/23/18.
+//  Copyright © 2018 Jay Clark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func buttonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "goToSecondScreen", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToSecondScreen" {
+            let destinationVC = segue.destination as! SecondViewController
+            
+            destinationVC.textPassedOver = textField.text
+            
+        }
+    }
+}
+
