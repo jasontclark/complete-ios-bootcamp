72c170436c804143980ffeec587579c742980825
How, Swift? <github@howswift.com>
2018-02-26 04:41:20 -0500

    Added the Delegate and Protocol for passing the changed city back to the WeatherVC.

--- xcode-projects/xcode-clima-app/Clima/ChangeCityViewController.swift
+++ xcode-projects/xcode-clima-app/Clima/ChangeCityViewController.swift
@@ -10,13 +10,14 @@ import UIKit
 
 
 //Write the protocol declaration here:
-
-
+protocol ChangeCityDelegate {
+    func userEnteredANewCityName(city: String)
+}
 
 class ChangeCityViewController: UIViewController {
     
     //Declare the delegate variable here:
-
+    var delegate : ChangeCityDelegate?
     
     //This is the pre-linked IBOutlets to the text field:
     @IBOutlet weak var changeCityTextField: UITextField!
@@ -25,15 +26,14 @@ class ChangeCityViewController: UIViewController {
     //This is the IBAction that gets called when the user taps on the "Get Weather" button:
     @IBAction func getWeatherPressed(_ sender: AnyObject) {
         
-        
-        
         //1 Get the city name the user entered in the text field
-        
-        
-        //2 If we have a delegate set, call the method userEnteredANewCityName
-        
+        if let cityName = changeCityTextField.text {
+            //2 If we have a delegate set, call the method userEnteredANewCityName
+            delegate?.userEnteredANewCityName(city: cityName)
+        }
         
         //3 dismiss the Change City View Controller to go back to the WeatherViewController
+        self.dismiss(animated: true, completion: nil)
         
         
     }
