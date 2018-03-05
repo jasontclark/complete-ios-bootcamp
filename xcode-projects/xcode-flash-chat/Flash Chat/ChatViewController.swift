d4a7d70b1c8def38fd061ece557f84eca81789cb
How, Swift? <github@howswift.com>
2018-03-05 04:49:43 -0500

    Added the login and logout features to the flash-chat app.

--- xcode-projects/xcode-flash-chat/Flash Chat/ChatViewController.swift
+++ xcode-projects/xcode-flash-chat/Flash Chat/ChatViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Firebase
 
 class ChatViewController: UIViewController {
     
@@ -107,6 +107,13 @@ class ChatViewController: UIViewController {
     @IBAction func logOutPressed(_ sender: AnyObject) {
         
         //TODO: Log out the user and send them back to WelcomeViewController
+        
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch {
+            print("Error, there was a problem signing out.")
+        }
         
         
     }
--- xcode-projects/xcode-flash-chat/Flash Chat/LogInViewController.swift
+++ xcode-projects/xcode-flash-chat/Flash Chat/LogInViewController.swift
@@ -6,7 +6,7 @@
 
 
 import UIKit
-
+import Firebase
 
 class LogInViewController: UIViewController {
 
@@ -28,7 +28,14 @@ class LogInViewController: UIViewController {
 
         
         //TODO: Log in the user
-        
+        Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+            if error != nil {
+                print(error!)
+            } else {
+                print("Login Successful!")
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
         
     }
     
