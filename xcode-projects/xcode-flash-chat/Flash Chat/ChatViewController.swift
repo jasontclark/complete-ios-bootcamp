f27ca2ea64fb2210d64a23c92c51812758835882
How, Swift? <github@howswift.com>
2018-03-05 05:36:08 -0500

    Beginning the setup of the TableView inside the ChatViewController. Setup the UITableViewDelegate and UITableViewDataSource methods. Used the CustomMessageCell for each row in the TableView. Configured the tableview to trigger the custom contraints on each cell.

--- xcode-projects/xcode-flash-chat/Flash Chat/ChatViewController.swift
+++ xcode-projects/xcode-flash-chat/Flash Chat/ChatViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Firebase
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     // Declare instance variables here
 
@@ -26,7 +26,8 @@ class ChatViewController: UIViewController {
         super.viewDidLoad()
         
         //TODO: Set yourself as the delegate and datasource here:
-        
+        messageTableView.delegate = self
+        messageTableView.dataSource = self
         
         
         //TODO: Set yourself as the delegate of the text field here:
@@ -38,7 +39,13 @@ class ChatViewController: UIViewController {
         
 
         //TODO: Register your MessageCell.xib file here:
-
+        messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
+        
+        // Call the configureTableView func
+        // to trigger the custom constraints that
+        // allow the cell to expand based on the
+        // message size/length.
+        configureTableView()
         
     }
 
@@ -49,11 +56,25 @@ class ChatViewController: UIViewController {
     
     
     //TODO: Declare cellForRowAtIndexPath here:
-    
-    
-    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // For all the rows of data, give the TableView a custom cell to display the data.
+        let cell = tableView.dequeueReusableCell(withIdentifier:
+            "customMessageCell", for: indexPath) as! CustomMessageCell
+        
+        // Creates a test message array. Each time this func is called,
+        // this array gets created.
+        let messageArray = ["First Message", "Second Message", "Third Message"]
+        
+        // The indexPath.row is used to pick out a message
+        // from the messageArray.
+        cell.messageBody.text = messageArray[indexPath.row]
+        
+        return cell
+    }
     //TODO: Declare numberOfRowsInSection here:
-    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
     
     
     //TODO: Declare tableViewTapped here:
@@ -61,7 +82,10 @@ class ChatViewController: UIViewController {
     
     
     //TODO: Declare configureTableView here:
-    
+    func configureTableView() {
+        messageTableView.rowHeight = UITableViewAutomaticDimension
+        messageTableView.estimatedRowHeight = 120.00
+    }
     
     
     ///////////////////////////////////////////
