1ddf7786fd9a78ff51f67dabe276eed1d2583396
How, Swift? <github@howswift.com>
2018-03-09 06:07:28 -0500

    Section 19 start. Initial creation of the Todoey to-do list app.

---
+++ xcode-projects/xcode-todoey-app/Todoey/Todoey/TodoListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Jay Clark on 3/9/18.
+//  Copyright © 2018 Jay Clark. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //MARK - Tableview Datasource Methods
+    
+}
+
