4b68b57108f7dbe6be81e516edc0e21d91b0d934
How, Swift? <github@howswift.com>
2018-03-12 05:26:37 -0400

    Added the TableView datasource and delegate methods to add ToDoItems to the TableView list, determine the number of rows in the list, and select individual ToDoItems in the list to check/uncheck them.

--- xcode-projects/xcode-todoey-app/Todoey/Todoey/TodoListViewController.swift
+++ xcode-projects/xcode-todoey-app/Todoey/Todoey/TodoListViewController.swift
@@ -17,7 +17,35 @@ class TodoListViewController: UITableViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    //MARK - Tableview Datasource Methods
+    //MARK - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
+    }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    //Mark - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            // When the checked TodoItem is selected, remove checkmark
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            // When the unchecked TodoItem is selected, add a checkmark
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        // Animate selection
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
