b989d65bd44c2ae9db7fc9967e9569b6e05c4255
How, Swift? <github@howswift.com>
2018-03-13 05:47:26 -0400

    Added the 'Add Item' button to the TodoListViewController. Pops up a 'Add New Item' alert when pressed. Adds the new item to the hard-coded itemArray.

--- xcode-projects/xcode-todoey-app/Todoey/Todoey/TodoListViewController.swift
+++ xcode-projects/xcode-todoey-app/Todoey/Todoey/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,6 +46,29 @@ class TodoListViewController: UITableViewController {
         
         // Animate selection
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //Mark - Add New Todoey Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            print(textField.text!)
+            self.itemArray.append(textField.text!)
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
     }
 }
 
