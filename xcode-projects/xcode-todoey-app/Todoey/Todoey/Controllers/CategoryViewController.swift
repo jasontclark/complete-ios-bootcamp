5bf2376f3f968381fb52965ecd39ef9de3e4003e
How, Swift? <github@howswift.com>
2018-04-11 06:44:11 -0400

    Section 19, Lesson 258 - Relationship Graphs in CoreData
    Didn't get to the relationship graph part yet. I had
    trouble with adding the new CategoryViewController to
    the main storyboard. But, I finally got that working.
    Next is the relationship graphs stuff.

---
+++ xcode-projects/xcode-todoey-app/Todoey/Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Jay Clark on 4/11/18.
+//  Copyright © 2018 Jay Clark. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        print("Todoey: Add Button Pressed!")
+    }
+    
+}
