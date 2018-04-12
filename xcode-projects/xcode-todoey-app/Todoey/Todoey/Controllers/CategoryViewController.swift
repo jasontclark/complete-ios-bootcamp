18e19ecd196f13c33c7b29e6ea46d69fa091ff5d
How, Swift? <github@howswift.com>
2018-04-12 05:33:38 -0400

    Section 19, Lecture 258 - Coding Challenge (WIP)
    This commit is a WIP. In the middle of setting up all of the
    TableView DataSource methods, and TableView Data manipulation
    methods for the Category tableview. Added a new entity to
    the CoreData datamodel document (Category) and setup the one
    to many relationship between a category and its assoc. items.

--- xcode-projects/xcode-todoey-app/Todoey/Todoey/Controllers/CategoryViewController.swift
+++ xcode-projects/xcode-todoey-app/Todoey/Todoey/Controllers/CategoryViewController.swift
@@ -7,17 +7,82 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryViewController: UITableViewController {
-
+    
+    
+    // Stores the Array of Categories
+    var categoryArray = [Category]()
+    
+    // Get a reference to the CoreData context
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Loads the Categories from
+        // persistent storage
+        loadCategories()
 
-      
     }
+    
+    // MARK: - TableView DataSource Methods
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return categoryArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        // Grab an Category from the categoryArray
+        let category = categoryArray[indexPath.row]
+        
+        // Set the textLabel in the cell
+        // to the name of the Category
+        cell.textLabel?.text = category.name
+        
+        return cell
+    }
+        
+    // MARK: - Data Manipulation Methods
+    
+    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        do {
+            // Use the context to execute the request,
+            // return the To-do Item objects,
+            // and store them in the Item array.
+            categoryArray = try context.fetch(request)
+        } catch {
+            print("Category Fetch Request error: \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    
+    func saveCategories() {
+        // Since the saving the data
+        // could throw an error, wrap this in a
+        // do-try-catch block
+        do {
+            // Saving Item data via CoreData
+            try context.save()
+        } catch {
+            print("Error saving Category context: \(error))")
+        }
+        
+        // Refresh the tableView to show the
+        // newly added item
+        self.tableView.reloadData()
+    }
+    
+    // MARK: - Add New Categories
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         print("Todoey: Add Button Pressed!")
     }
     
+    // MARK: - TableView Delegate Methods
 }
