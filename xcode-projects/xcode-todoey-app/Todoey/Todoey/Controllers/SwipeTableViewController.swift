d5a9b59072cb5daa69f044ed72a095ff7d898efd
How, Swift? <github@howswift.com>
2018-04-30 05:22:22 -0400

    Section 19, Lesson 269 - Inheriting from SwipeTableVC
    Started with adding the SwipeTableViewController file. This
    will be the superclass for both the CategoryVC and the TodoItemVC.

---
+++ xcode-projects/xcode-todoey-app/Todoey/Todoey/Controllers/SwipeTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SwipeTableViewController.swift
+//  Todoey
+//
+//  Created by Jay Clark on 4/30/18.
+//  Copyright © 2018 Jay Clark. All rights reserved.
+//
+
+import UIKit
+
+class SwipeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+}
