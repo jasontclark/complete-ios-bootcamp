cf34bd2a24778857eca689e01f0ecd6774aad32f
How, Swift? <github@howswift.com>
2018-05-17 07:33:28 -0400

    Section 21, Lecture 283 - Import Image Recognition Model
    Imported the ML model from developer.apple.com/machine-learning.

--- xcode-projects/xcode-see-food-app/SeeFood/SeeFood/ViewController.swift
+++ xcode-projects/xcode-see-food-app/SeeFood/SeeFood/ViewController.swift
@@ -7,19 +7,15 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
