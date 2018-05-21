959784eee8c9f1252dda577e061901aab658a91a
How, Swift? <github@howswift.com>
2018-05-21 05:07:46 -0400

    Section 21, Lecture 283 - Enable Camera
    This was fairly simple. Just added the camera button,
    the UIImageView object, and the delegate method
    for the UIImagePickerControllerDelegate to detect
    when an image has been 'picked' from the camera, i.e
    a picture was taken.

--- xcode-projects/xcode-see-food-app/SeeFood/SeeFood/ViewController.swift
+++ xcode-projects/xcode-see-food-app/SeeFood/SeeFood/ViewController.swift
@@ -12,10 +12,35 @@ import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+    }
+    
+    
+    // MARK - Image Picker Controller Deletage Methods
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // Using optional binding to downcast the
+        // dictionary value to a UIImage
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+          imageView.image = userPickedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
     }
 
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
 }
 
