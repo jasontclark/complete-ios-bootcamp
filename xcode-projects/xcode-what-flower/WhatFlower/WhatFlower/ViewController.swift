18b4ccd6859f2e69d0ac8c282e340dd128bb2b2f
How, Swift? <github@howswift.com>
2018-06-12 06:08:49 -0400

    Section 23, Lesson 301 - Flower Classifier Challenge
    Finished coding up the Flower Classifier project using
    a Caffe model converted into a CoreML model.

--- xcode-projects/xcode-what-flower/WhatFlower/WhatFlower/ViewController.swift
+++ xcode-projects/xcode-what-flower/WhatFlower/WhatFlower/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -19,7 +21,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         imagePicker.delegate = self
         imagePicker.sourceType = .camera
-        imagePicker.allowsEditing = false
+        imagePicker.allowsEditing = true
        
     }
 
@@ -31,11 +33,50 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     // MARK - UIImagePickerControllerDelegate methods
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
-            imageView.image = userPickedImage
+        if let userEditedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+            imageView.image = userEditedImage
+            
+            // Convert the Image from a UIImagePickerControllerEditedImage
+            // to a CIImage
+            guard let ciimage = CIImage(image: userEditedImage) else {
+                fatalError("Could not convert UIImage to CIImage!")
+            }
+            
+            detectFlower(withImage: ciimage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK - CoreML Flower Detection method
+    func detectFlower(withImage image: CIImage) {
+        
+        // Load the FlowerClassifier CoreML model
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Loading FlowerClassifier model failed!")
+        }
+        
+        // Create a Vision request
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image!")
+            }
+            
+            if let firstResult = results.first {
+                self.navigationItem.title = firstResult.identifier.capitalized
+            }
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image, options: [:])
+        
+        // Perform the request against the handler
+        // i.e the selected image
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
     }
     
 }
