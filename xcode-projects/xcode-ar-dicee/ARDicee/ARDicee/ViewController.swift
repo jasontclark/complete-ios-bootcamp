5a6e23c5e659a59ed89bce56e3ee5547443e9b5d
How, Swift? <github@howswift.com>
2018-06-25 06:38:37 -0400

    Section 24, Lesson 314 - Setting up ARDicee
    Created a new ARKit project called ARDicee to begin
    this new section on augmented reality. This project
    will be a 3D AR version of the Dicee app that we
    created early on in the course.

---
+++ xcode-projects/xcode-ar-dicee/ARDicee/ARDicee/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  ARDicee
+//
+//  Created by Jay Clark on 6/25/18.
+//  Copyright © 2018 Jay Clark. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+}
